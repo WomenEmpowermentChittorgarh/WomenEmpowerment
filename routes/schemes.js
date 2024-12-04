0b993c0e37e381cdb29270ecc7a9f748b17904f1
Mohit Bhatnagar <bhatnagarmohit11@gmail.com>
2024-12-05 00:18:55 +0530

    scheme API changes

--- routes/schemes.js
+++ routes/schemes.js
@@ -32,7 +32,7 @@ router.get('/fetch-schemes', VerifyUserToken, (req, res) => {
             console.error("Database error:", err);
             return res.status(500).json(responseHandler("Failure", 500, "Internal Server Error"));
         }
-        res.status(200).json(responseHandler("Success", 200, "Schemes Fetched successfully", { data }));
+        res.status(200).json(responseHandler("Success", 200, "Schemes Fetched successfully", { data.id, data.scheme_name, data.started_date }));
     });
 });
 
