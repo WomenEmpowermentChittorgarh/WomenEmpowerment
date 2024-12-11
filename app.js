faabb3c735c391a545461f1702aa1b0d78e2befa
himanshubhatnagar-nfd <himanshu.bhatnagar@nexflaredynamics.com>
2024-12-11 23:02:41 +0530

    excel

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Route imports
 const userRoutes = require('./routes/users');
